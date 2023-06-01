8e7ca96140cfdacaefb0ce0f6c46c06828a0ddde
Diego Vistoso <diegoivg98@gmail.com>
2023-06-01 15:54:06 -0400

    quitar mensajes echo a conexion bd

--- conexion.php
+++ conexion.php
@@ -1,10 +1,3 @@
 <?php
 
 $conexion = pg_pconnect("host='localhost' port='5432' dbname='externo2' user='postgres' password='fuciante98'");
-
-if (!$conexion) {
-      echo "Error al conectar a la base de datos.";
-      exit;
-   }else{
-    echo "Conexion exitosa";
-   }
