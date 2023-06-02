9e9bb7b1a4d5557d722b03d6348c17027b7fe10e
Diego Vistoso <diegoivg98@gmail.com>
2023-06-02 18:37:38 -0400

    agregar query para obtener candidatos relacionados a sus comunas

---
+++ getCandidato.php
@@ -0,0 +1,12 @@
+<?php
+include_once('./conexion.php');
+
+$comuna = $_GET['id_comuna'];
+$query = pg_query($conexion, "SELECT comuna.id_comuna, candidato.nom_candidato
+                              FROM public.comuna
+                              INNER JOIN public.candidato ON comuna.id_comuna = candidato.comuna
+                              WHERE comuna = $comuna");
+
+while ($row = pg_fetch_row($query)) {
+    echo "<option value='" . $row[0] . "'>" . $row[1] . "</option>";
+}
