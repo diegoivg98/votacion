7a51cbfb882647163b01a25e4382dfff1ee0653a
Diego Vistoso <diegoivg98@gmail.com>
2023-06-02 18:38:00 -0400

    cambios en la query de comunas

--- getComuna.php
+++ getComuna.php
@@ -1,12 +1,13 @@
 <?php
 include_once('./conexion.php');
 
-$id_region = $_GET['id_region'];
-$query = pg_query($conexion, "SELECT region.id_region, comuna.nom_comuna
-                              FROM public.comuna 
-                              INNER JOIN public.region ON region.id_region = comuna.region 
-                              WHERE region = $id_region");
+$region = $_GET['region'];
+$query = pg_query($conexion, "SELECT comuna.id_comuna, comuna.nom_comuna
+                              FROM comuna 
+                              INNER JOIN region ON region.id_region = comuna.region 
+                              WHERE id_region = $region");
 
-while ($row = pg_fetch_row($query)) {
-    echo "<option value='" . $row[0] . "'>" . $row[1] . "</option>";
+while ($row = pg_fetch_assoc($query)) {
+    echo "<option value='" . $row['id_comuna'] . "'>" . $row['nom_comuna'] . "</option>";
 }
+?>
