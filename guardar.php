baae3cd833d9d9d1c32c1ceacb454d2056088940
Diego Vistoso <diegoivg98@gmail.com>
2023-06-05 02:36:37 -0400

    arreglo en variable alias y query de insert

--- guardar.php
+++ guardar.php
@@ -1,24 +1,29 @@
 <?php
-
-include './conexion.php';
+include 'conexion.php';
 
 $rut = $_POST['rut'];
 $nombre = $_POST['nombre'];
-$aliass = $_POST['aliass'];
-$email = $_POST['email'];
+$alias = $_POST['alias'];
+$email = $_POST['correo'];
 $region = $_POST['region'];
 $comuna = $_POST['comuna'];
 $candidato = $_POST['candidato'];
-$nosotros = implode(',', $_POST['nosotros']);
+$recomendaciones = implode(',', $_POST['recomendaciones']);
 
 // Realizar una consulta para verificar si ya existe rut y/o alias
-$query = "SELECT * FROM votante WHERE rut = '$rut' AND alias = '$alias'";
-$result = pg_query($conexion, $query);
+$duplicados = "SELECT * FROM votante WHERE rut = '$rut' AND alias = '$alias'";
+$result = pg_query($conexion, $duplicados);
 
 if (pg_num_rows($result) > 0) {
     // Registro duplicado encontrado, mostrar mensaje de error o realizar alguna acción
     echo "<script type='text/javascript'>alert('Error: El registro ya existe en la base de datos.');</script>";
 } else {
-    $query = "INSERT INTO votante (rut, nombre, alias, email, region, comuna, candidato, nosotros) VALUES ($rut, $nombre, $alias, $email, $region, $comuna, $candidato, $nosotros)";
-    pg_query($conexion, $query);
+    $query = "INSERT INTO votante (rut, nombre, alias, email, region, comuna, candidato, recomendaciones) 
+              VALUES ('$rut', '$nombre', '$alias', '$email', '$region', '$comuna', '$candidato', '$recomendaciones')";
+    $result = pg_query($conexion, $query);
+    if (!$result) {
+        echo "Error al ejecutar la consulta: " . pg_last_error($conexion);
+    } else {
+        echo "La inserción se realizó correctamente.";
+    }
 }
