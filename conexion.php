7738e8abc778518c10a430137f7ecacf33d7bfd2
Diego Vistoso <diegoivg98@gmail.com>
2023-06-05 02:38:03 -0400

    agregar validacion de conexion a BD

--- conexion.php
+++ conexion.php
@@ -1,3 +1,7 @@
 <?php
 
 $conexion = pg_pconnect("host='localhost' port='5432' dbname='externo2' user='postgres' password='fuciante98'");
+
+if (!$conexion) {
+    die('Error de conexión a la base de datos: ' . pg_last_error());
+}
