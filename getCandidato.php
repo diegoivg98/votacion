8868632ed0de5fcf10e435d219bee523ff19f5e3
Diego Vistoso <diegoivg98@gmail.com>
2023-06-05 01:05:12 -0400

    cambios en la obtencion de comuna

--- getCandidato.php
+++ getCandidato.php
@@ -1,7 +1,7 @@
 <?php
 include_once('./conexion.php');
 
-$comuna = $_GET['id_comuna'];
+$comuna = $_GET['comuna'];
 $query = pg_query($conexion, "SELECT comuna.id_comuna, candidato.nom_candidato
                               FROM public.comuna
                               INNER JOIN public.candidato ON comuna.id_comuna = candidato.comuna
