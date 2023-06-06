be5a5ca5b1262ca6143a5b3d250f961fa89fcb0c
Diego Vistoso <diegoivg98@gmail.com>
2023-06-05 22:20:01 -0400

    actualizacion en linea de conexion a BD

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = pg_pconnect("host='localhost' port='5432' dbname='externo2' user='postgres' password='fuciante98'");
+$conexion = pg_pconnect("host='localhost' port='5432' dbname='tubasededatos' user='tuusuario' password='tucontraseña'");
 
 if (!$conexion) {
     die('Error de conexión a la base de datos: ' . pg_last_error());
